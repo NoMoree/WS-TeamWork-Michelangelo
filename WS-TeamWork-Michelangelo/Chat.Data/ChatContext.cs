d36c4cb29d9ce1047e4246de4ea0dabbabccff5c
NoMoree <u8q18@abv.bg>
2013-08-14 17:12:27 +0300

    a

--- WS-TeamWork-Michelangelo/Chat.Data/ChatContext.cs
+++ WS-TeamWork-Michelangelo/Chat.Data/ChatContext.cs
@@ -10,28 +10,28 @@ namespace Chat.Data
 {
     public class ChatContext
     {
-        public ChatContext()
-            : base(ConfigurationManager.AppSettings[""])
-        {
-        }
+        //public ChatContext()
+        //    : base("asdasdas")//ConfigurationManager.AppSettings[""])
+        //{
+        //}
 
         public DbSet<Message> Messages { get; set; }
         public DbSet<User> Users { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            //Users
-            modelBuilder.Entity<User>().HasKey(x => x.Id);
-            modelBuilder.Entity<User>().Property(x => x.Username).IsRequired().HasMaxLength(50);
-            modelBuilder.Entity<User>().Property(x => x.Password).IsRequired().HasMaxLength(16);
-            modelBuilder.Entity<User>().Property(x => x.Nickname).IsRequired().HasMaxLength(50);
-            modelBuilder.Entity<User>().Property(x => x.SessionKey).HasMaxLength(50);
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    //Users
+        //    modelBuilder.Entity<User>().HasKey(x => x.Id);
+        //    modelBuilder.Entity<User>().Property(x => x.Username).IsRequired().HasMaxLength(50);
+        //    modelBuilder.Entity<User>().Property(x => x.Password).IsRequired().HasMaxLength(16);
+        //    modelBuilder.Entity<User>().Property(x => x.Nickname).IsRequired().HasMaxLength(50);
+        //    modelBuilder.Entity<User>().Property(x => x.SessionKey).HasMaxLength(50);
 
-            //Messages
-            modelBuilder.Entity<Message>().HasKey(x => x.Id);
-            modelBuilder.Entity<Message>().Property(x => x.Content).HasColumnType("text");
+        //    //Messages
+        //    modelBuilder.Entity<Message>().HasKey(x => x.Id);
+        //    modelBuilder.Entity<Message>().Property(x => x.Content).HasColumnType("text");
 
-            base.OnModelCreating(modelBuilder);
-        }
+        //    base.OnModelCreating(modelBuilder);
+        //}
     }
 }
